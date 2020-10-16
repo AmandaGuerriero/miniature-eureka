776661d5862918941661663cb6182a90852ba037
Amanda Guerriero <ag.codingclass@gmail.com>
2020-10-16 14:54:17 -0700

    Post and return notes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { notes } = require('./data/notes.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const router = require('express').Router();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -12,18 +13,32 @@ app.use(express.json());
 // Make files available
 app.use(express.static('public'));
 
+// Create a new note
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './data/notes.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return notes;
+  }
+  
+
+// Validate the Note has the proper information
+function validateNote (note) {
+    if(!note.title || typeof note.title !== 'string') {
+        return false
+    }
+    if(!note.text || typeof note.text !== 'string') {
+        return false
+    }
+    return true
+}
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
-
-// app.get('/api/animals', (req, res) => {
-//     let results = animals;
-//     if (req.query) {
-//         results = filterByQuery(req.query, results);
-//     }
-//     res.json(results);
-// });
 
 // send to index
 app.get('/', (req, res) => {
@@ -33,6 +48,19 @@ app.get('/', (req, res) => {
 // send to notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    // Send 400 error if any data req is not passed
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted');
+    } else {
+    // add note to json file
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+    }
 });
 
 // server setup
